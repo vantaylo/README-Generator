70beabddbdfc11f644de34d3fd7271c2174fe4ef
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-06-26 17:40:02 -0500

    updated questions

--- index.js
+++ index.js
@@ -15,29 +15,19 @@ function askQuestion(currentQuestion) {
 }
 
 async function main() {
-  const badge = await askQuestion(
-    "Are you sure you want to deploy to PRODUCTION? "
-  );
-
-  const title = await askQuestion("What is the title of your application? ");
-
+  const title = await askQuestion("What is the title of the application?");
+  const badge = await askQuestion("Want to include a badge?");
   const description = await askQuestion(
-    "Please provide a description for the application? "
+    "Please provide a description for the application?"
   );
-
   const tableOfContents = await askQuestion("What is the table of contents?");
-
-  const installation = await askQuestion(
-    "How do you install the application? "
+  const installation = await askQuestion("How do you install the application?");
+  const usage = await askQuestion("Provide any examples of usage.");
+  const license = await askQuestion("A short description about the license.");
+  const contributing = await askQuestion(
+    "Provide the description about what people can add in your project."
   );
-
-  const usage = await askQuestion("");
-
-  const license = await askQuestion("");
-
-  const contributing = await askQuestion("");
-
-  const test = await askQuestion("");
+  const test = await askQuestion("Provide example how to run the code.");
 
   console.log(ans);
 }
